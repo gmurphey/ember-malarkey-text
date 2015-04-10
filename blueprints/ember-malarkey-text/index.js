35d103cefe72f5bd0f17e4cae0c432eef609430f
Garrett Murphey <gmurphey@gmurphey.com>
2015-04-09 20:05:20 -0400

    Fixing blueprint file.

--- blueprints/ember-malarkey-text/index.js
+++ blueprints/ember-malarkey-text/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   description: 'Add ember-malarkey-text bower dependencies to app.',
 
+  normalizeEntityName: function () {},
+
   afterInstall: function () {
     return this.addBowerPackagesToProject([
       { name: 'yuanqing/malarkey' }
